1732d01e9738767cc84aece059b3846c2e4547fd
RGalic99 <roko.gali7@gmail.com>
2020-11-04 13:11:53 +0100

    Login

--- postgres-express-node/api/index.js
+++ postgres-express-node/api/index.js
@@ -3,10 +3,10 @@ const router = express.Router();
 
 const hello = require("./routes/hello");
 const user = require("./routes/user");
-
+const login = require("./routes/login");
 module.exports = () => {
   hello(router);
   user(router);
-
+  login(router);
   return router;
 };
---
+++ postgres-express-node/api/routes/login.schemas.js
@@ -0,0 +1,11 @@
+
+  exports.login = {
+    title: "Login parameters",
+    type: "object",
+    properties: {
+      username: { type: "string", minLength: 1 },
+      password: { type: "string", minLength: 1 },
+    },
+    required: ["username", "password"],
+  };
+  
