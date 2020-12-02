e5fd3ab743d25719222e31b480bdfc349ab9d250
RGalic99 <roko.gali7@gmail.com>
2020-12-02 13:13:21 +0100

    Fixed password comparison

--- postgres-express-node/services/login.service.js
+++ postgres-express-node/services/login.service.js
@@ -1,3 +1,4 @@
+const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const config = require("../config");
 
@@ -25,7 +26,8 @@ class LoginService {
 		}
 
 		this.logger.info("Checking password");
-		if (userRecord.password === password) {
+		const validPassword = await bcrypt.compare(password, userRecord.password);
+		if (validPassword) {
 			this.logger.info("Password correct");
 
 			const user = {
