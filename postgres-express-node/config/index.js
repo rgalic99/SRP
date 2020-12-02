c5f5bd1589720767993337059f796ca0fb55fba4
RGalic99 <roko.gali7@gmail.com>
2020-12-02 10:29:52 +0100

    Add JWT token verification using express-jwt

--- postgres-express-node/config/index.js
+++ postgres-express-node/config/index.js
@@ -30,5 +30,14 @@ module.exports = {
 	jwt: {
 		secret: process.env.JWT_SECRET,
 		expiresIn: process.env.JWT_DURATION || "1h",
+		algorithms: ["HS256"],
+		exclude: {
+			path: [
+				{
+					url: "/api/login",
+					methods: ["POST"],
+				},
+			],
+		},
 	},
 };
--- postgres-express-node/loaders/express.js
+++ postgres-express-node/loaders/express.js
@@ -1,41 +1,52 @@
 const express = require("express");
+const jwt = require("express-jwt");
 const cors = require("cors");
 const config = require("../config");
 const routes = require("../api");
 module.exports = ({ app, HttpLogger: logger }) => {
-  //---------------------------
-  // REGISTER MIDDLEWARE
-  // (Remember that the order in
-  //  which you use the middleware
-  //  matters.)
-  //---------------------------
-  app.use(logger);
-  app.use(cors());
-  app.use(express.json());
-  app.use(express.urlencoded({ extended: false }));
+	//---------------------------
+	// REGISTER MIDDLEWARE
+	// (Remember that the order in
+	//  which you use the middleware
+	//  matters.)
+	//---------------------------
+	app.use(logger);
+	app.use(cors());
+	app.use(express.json());
+	app.use(express.urlencoded({ extended: false }));
+	app.use(
+		jwt({
+			algorithms: config.jwt.algorithms,
+			secret: config.jwt.secret,
+		}).unless(config.jwt.exclude)
+	);
+	//---------------------------
+	// LOAD/MOUNT API ROUTES
+	// (path prefix e.g. /api)
+	//---------------------------
+	app.use(config.api.prefix, routes());
 
-  //---------------------------
-  // LOAD/MOUNT API ROUTES
-  // (path prefix e.g. /api)
-  //---------------------------
-  app.use(config.api.prefix, routes());
+	//---------------------------
+	// ERROR HANDLERS
+	//---------------------------
+	// catch 404 and forward to error handler
+	app.use((req, res, next) => {
+		const err = new Error("Not Found");
+		err.status = 404;
+		next(err);
+	});
 
-  //---------------------------
-  // ERROR HANDLERS
-  //---------------------------
-  // catch 404 and forward to error handler
-  app.use((req, res, next) => {
-    const err = new Error("Not Found");
-    err.status = 404;
-    next(err);
-  });
+	// ultimate error handler
+	app.use((err, req, res, next) => {
+		if (err.name === "Unathorized") {
+			err.status = 401;
+			err.message = "Not authorized (invalid token)";
+		}
 
-  // ultimate error handler
-  app.use((err, req, res, next) => {
-    res.status(err.status || 500).json({
-      error: {
-        message: err.message || "Internal Server Error",
-      },
-    });
-  });
+		res.status(err.status || 500).json({
+			error: {
+				message: err.message || "Internal Server Error",
+			},
+		});
+	});
 };
