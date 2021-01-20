07b355157142cf0de88e29eca3f597d176dac435
Roko Galić <roko.gali7@gmail.com>
2021-01-20 10:48:43 +0100

    Used CASL

--- postgres-express-node/config/authorization-rules.js
+++ postgres-express-node/config/authorization-rules.js
@@ -7,86 +7,86 @@
 const { AbilityBuilder, Ability, ForbiddenError } = require("@casl/ability");
 
 ForbiddenError.setDefaultMessage(
-  (error) => `Not authorized for "${error.action}" on "${error.subjectType}"`
+	(error) => `Not authorized for "${error.action}" on "${error.subjectType}"`
 );
 
 let ANONYMOUS_ABILITY;
 
 function defineAbilityFor(user) {
-  if (user) {
-    return new Ability(defineRulesFor(user));
-  }
+	if (user) {
+		return new Ability(defineRulesFor(user));
+	}
 
-  ANONYMOUS_ABILITY = ANONYMOUS_ABILITY || new Ability(defineRulesFor({}));
-  return ANONYMOUS_ABILITY;
+	ANONYMOUS_ABILITY = ANONYMOUS_ABILITY || new Ability(defineRulesFor({}));
+	return ANONYMOUS_ABILITY;
 }
 
 function defineRulesFor(user) {
-  const builder = new AbilityBuilder(Ability);
+	const builder = new AbilityBuilder(Ability);
 
-  switch (user.role) {
-    case "admin":
-      defineAdminRules(builder);
-      break;
-    case "user":
-      defineAnonymousRules(builder);
-      defineUserRules(builder, user);
-      break;
-    case "doctor":
-      defineAnonymousRules(builder);
-      defineUserRules(builder, user);
-      defineDoctorRules(builder);
-      break;
-    default:
-      defineAnonymousRules(builder);
-      break;
-  }
+	switch (user.role) {
+		case "admin":
+			defineAdminRules(builder);
+			break;
+		case "user":
+			defineAnonymousRules(builder);
+			defineUserRules(builder, user);
+			break;
+		case "doctor":
+			defineAnonymousRules(builder);
+			defineUserRules(builder, user);
+			defineDoctorRules(builder);
+			break;
+		default:
+			defineAnonymousRules(builder);
+			break;
+	}
 
-  return builder.rules;
+	return builder.rules;
 }
 
 // =============================
 // "Roles-permissions" mapping
 // -----------------------------
 function defineAdminRules({ can }, user) {
-  can("manage", "all");
+	can("manage", "all");
 }
 
 function defineDoctorRules({ can }, user) {
-  // We allow doctors read access to all MedicalTests.
-  // To implement this policy in CASL we use a simple
-  // trick of merely checking if field UserId field
-  // exists in the given MedicalTest object (we do not
-  // verify the owner of the given test as in the case
-  // of regular users).
-  //
-  // To learn more about the used operator "$exists"
-  // please check: https://casl.js.org/v4/en/guide/conditions-in-depth
-  can("read", "MedicalTest", { UserId: { $exists: true } });
+	// We allow doctors read access to all MedicalTests.
+	// To implement this policy in CASL we use a simple
+	// trick of merely checking if field UserId field
+	// exists in the given MedicalTest object (we do not
+	// verify the owner of the given test as in the case
+	// of regular users).
+	//
+	// To learn more about the used operator "$exists"
+	// please check: https://casl.js.org/v4/en/guide/conditions-in-depth
+	can("read", "MedicalTest", { UserId: { $exists: true } });
 }
 
 function defineUserRules({ can }, user) {
-  // entity User
-  can(["read", "delete"], "User", { id: user.id });
-  can(["update"], "User", ["username", "password"], {
-    id: user.id,
-  });
+	// entity User
+	can(["read", "delete"], "User", { id: user.id });
+	can(["update"], "User", ["username", "password"], {
+		id: user.id,
+	});
 
-  // entity MedicalTest
-  can(["create", "read", "delete"], "MedicalTest", {
-    UserId: user.id,
-  });
-  can(["update"], "MedicalTest", ["name", "result"], {
-    UserId: user.id,
-  });
+	// entity MedicalTest
+	can(["create", "read", "delete"], "MedicalTest", {
+		UserId: user.id,
+	});
+	can(["update"], "MedicalTest", ["name", "result"], {
+		UserId: user.id,
+	});
 }
 
 function defineAnonymousRules({ can }, user) {
-  // entity User
-  can("create", "User");
+	// entity User
+	can("create", "User");
 }
 
 module.exports = {
-  defineRulesFor,
-  defineAbilityFor,
+	defineRulesFor,
+	defineAbilityFor,
 };
